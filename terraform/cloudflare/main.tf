a4b12321ecdf45265a8ef74cea2271e2e158f7cb
Adam <adam@ajclarkson.co.uk>
2026-06-21 15:37:54 +0100

    ci: fix yamllint errors - trailing spaces, comma spacing, exclude gotk-components

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -8,13 +8,11 @@ terraform {
     }
   }
 
-  backend "http" {
-    address        = "https://gitlab.com/api/v4/projects/75240401/terraform/state/cloudflare"
-    lock_address   = "https://gitlab.com/api/v4/projects/75240401/terraform/state/cloudflare/lock"
-    unlock_address = "https://gitlab.com/api/v4/projects/75240401/terraform/state/cloudflare/lock"
-    lock_method    = "POST"
-    unlock_method  = "DELETE"
-    retry_wait_min = 5
+  cloud {
+    organization = "ajclarkson"
+    workspaces {
+      name = "cloudflare"
+    }
   }
 }
 
